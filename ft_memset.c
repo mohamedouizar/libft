a8da5f3458c64fdad6bd632096f83aa0bffff4ab
Mohamed Ouizar <mouizar@e2r6p5.1337.ma>
2021-11-07 19:16:26 +0100

    memst header

--- ft_memset.c
+++ ft_memset.c
@@ -1,18 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouizar <mouizar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 19:15:53 by mouizar           #+#    #+#             */
+/*   Updated: 2021/11/07 19:15:54 by mouizar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len);
 {
 	unsigned char	*p;
 
 	p = (unsigned char *)b;
 	while (len--)
+	{
 		*p++ = (unsigned char) c;
+	}
 	return (b);
 }
-
-int	main(void)
+/*
+int main (void)
 {
-	char *str = strdup("Hello my name is ouizar ");
-	printf("|%s|", ft_memset(str ,'*',5));
+	  char str[] = "mohamed ouizar";
+	 printf("%s/n",ft_memset(str,'*',3));
 }
+*/
--- libft.h
+++ libft.h
@@ -5,4 +5,5 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
 #endif
