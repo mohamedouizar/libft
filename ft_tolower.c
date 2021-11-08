0d032933a0fbcd9f38eb954b11fceade507621c2
Mohamed Ouizar <mouizar@e2r6p5.1337.ma>
2021-11-08 14:57:35 +0100

    upperlower

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mouizar <mouizar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/08 14:31:19 by mouizar           #+#    #+#             */
-/*   Updated: 2021/11/08 14:31:29 by mouizar          ###   ########.fr       */
+/*   Created: 2021/11/08 14:29:40 by mouizar           #+#    #+#             */
+/*   Updated: 2021/11/08 14:29:59 by mouizar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 //#include <ctype.h>
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
 	unsigned char	cc;
 
 	cc = (unsigned char) c;
-	if (cc >= 'a' && cc <= 'z')
-		return (cc = cc - 32);
+	if (cc >= 'A' && cc <= 'Z')
+		return (cc = cc + 32);
 	return (c);
 }
 /*
 int	main(void)
 {
-	printf("%c", ft_toupper('@'));
+	printf("%c", ft_tolower('R'));
 }
 */
--- libft.h
+++ libft.h
@@ -7,4 +7,7 @@ int	ft_isalnum(int c);
 int	ft_isprint(int c);
 void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
 #endif
